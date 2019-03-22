69bc4e7c9988eb82c8fb302d65d47f25e3dd65c1
Chris Nguyen <34232894+nguyenchris@users.noreply.github.com>
2019-03-21 17:40:34 -0700

    config webpack

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/dist'))
+
+app.get('/', (req, res) => {
+  res.send('hi');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`);
+});
